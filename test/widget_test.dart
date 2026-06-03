40bb6fb1fa71926fc3a3e5e61c06c4d6764fcbf9
farah <farahsyaz2105@gmail.com>
2026-06-03 23:17:54 +0700

    nyoba doang

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,22 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:lapor_keuangan
-/main.dart';
+// Baris di bawah ini sudah disatukan agar tidak eror
+import 'package:lapor_keuangan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Membangun aplikasi dan memicu frame pertama.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
+    // Catatan: Test di bawah ini mungkin akan gagal jika aplikasi 
+    // Money Tracker Anda tidak memiliki teks '0' atau tombol '+'
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
